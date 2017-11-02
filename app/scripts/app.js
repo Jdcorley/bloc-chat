0b5f0ee0beff001ddce4c5bf4b115b5ab8cc754f
Jack <jdcorley@gmail.com>
2017-11-02 17:03:21 -0400

    stying changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,10 +11,10 @@
                   url: '/',
                   controller: 'HomeCtrl as homeCtrl',
                   templateUrl: '/templates/home.html'
-              });  
+            });  
       }
 
   angular
-    .module('blocChat', ['ui.router', 'firebase'])
+    .module('blocChat', ['ui.router', 'firebase','ui.bootstrap'])
     .config(config);
 })();
