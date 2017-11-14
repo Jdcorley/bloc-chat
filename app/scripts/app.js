8f78f3b3e5506d016869ed50ff26c6c20a21649c
Jack <jdcorley@gmail.com>
2017-11-14 11:24:35 -0500

    checkpoint-5-complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,16 +5,35 @@
                   enabled: true,
                   requireBase: false
                });
-
+/*ui route for home ctrl and home template*/
           $stateProvider
               .state('home', {
                   url: '/',
                   controller: 'HomeCtrl as homeCtrl',
                   templateUrl: '/templates/home.html'
             });
+   
       }
-
+      function BlocChatCookies($cookies, $uibModal) {
+        var currentUser = $cookies.get('blocChatCurrentUser');
+        console.log(currentUser);
+        if (!currentUser || currentUser === undefined) {
+            var modalInstance = $uibModal.open({
+              templateUrl: '/templates/modalUsername.html',
+              controller: 'ModalUsernameCtrl',
+              controllerAs: 'modalUsernameCtrl'
+            });
+            modalInstance.result.then (function(username){
+               this.username = username; 
+               $cookies.put('blocChatCurrentUser',username) 
+              }, function(){
+                console.log("You have to add a user");
+              }) 
+      }
+    }
+/* dependencies ui router, firebase, bootstrap; module blocChat; config */
   angular
-    .module('blocChat', ['ui.router', 'firebase','ui.bootstrap'])
-    .config(config);
+    .module('blocChat', ['ui.router', 'firebase','ui.bootstrap','ngCookies'])
+    .config(config)
+    .run(['$cookies','$uibModal', BlocChatCookies]);
 })();
--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,5 +1,5 @@
 (function (){
-  function HomeCtrl(Room, $uibModal){
+  function HomeCtrl(Room, $uibModal, $scope, Message){
     /**
      * open model instance 
      */
@@ -10,15 +10,24 @@
         controllerAs: 'modalCtrl', 
         bindToController: true
       }); 
+
       modalInstance.result.then (function(name){
         Room.add(name);
       }, function(){
         console.log("You canceled the modal");
       }) 
     }
+      //set active room
+      $scope.setActiveRoom = function(room){
+        $scope.activeRoom = room;
+        $scope.messages = Message.getByRoomId(room.$id); //Message service to filter messages by room $id
+      }
+  
       this.rooms = Room.all;
+      
+    
   }
   angular
   .module('blocChat')
-  .controller('HomeCtrl', ['Room','$uibModal', HomeCtrl]);
+  .controller('HomeCtrl', ['Room','$uibModal','$scope','Message', HomeCtrl]);
 })();
